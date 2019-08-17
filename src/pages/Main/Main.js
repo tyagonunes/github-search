2b80c16d80543128f4b9dc06d2c9666652ed6d69
Tyago Nunes <tyagonunes@gmail.com>
2019-08-17 11:41:48 -0300

    Logout of app

--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -7,47 +7,41 @@ import './Main.css';
 
 export default class Main extends Component {
 
-	constructor(props) {
-		super(props)
-
-		this.state = {
-			user: this.props.location.state.user,
-		}
-
-		console.log(this.state.user)
+	state = {
+		user: this.props.location.state.user,
 	}
 
 
-
 	render() {
+		const { user } = this.state;
+
 		return (
 			<div className="main-container">
 
 				<section className="container-profile">
 
 					<figure>
-						<img src={this.state.user.avatar_url} alt="avatar" />
+						<img src={user.avatar_url} alt="avatar" />
 					</figure>
 
 					<div className="profile-details">
 						<ul>
-							<li><span className="profile-nickname">{this.state.user.login}</span></li>
-							<li><span className="profile-bio">{this.state.user.bio}</span></li>
-							<li><span className="profile-email">{this.state.user.email}</span></li>
-							<li><a className="profile-link" href={this.state.user.html_url} target="blank">{this.state.user.html_url}</a></li>
+							<li><span className="profile-nickname">{user.login}</span></li>
+							<li><span className="profile-bio">{user.bio}</span></li>
+							<li><span className="profile-email">{user.email}</span></li>
+							<li><a className="profile-link" href={user.html_url} target="blank">{user.html_url}</a></li>
 						</ul>
-						<GoogleMap userLocation={this.state.user.location} />
-
-						<div className="main-logout">
-							<Link to="/">
-								<span>Sair</span>
-							</Link>
-						</div>
+						<GoogleMap userLocation={user.location} />
+					</div>
+					<div className="profile-logout">
+						<Link to="/" className="btn-primary"> 
+								Sair
+						</Link>
 					</div>
 				</section>
 
 				<section className="container-actions">
-					<Starreds userLogin={this.state.user.login} />
+					<Starreds userLogin={user.login} />
 				</section>
 			</div>
 		);
