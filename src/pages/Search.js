1a780d6539d3aa42188bc48087fd9d3c9bb3fe29
Tyago Nunes <tyagonunes@gmail.com>
2019-08-16 22:28:07 -0300

    validação form

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -6,38 +6,62 @@ import './Search.css';
 
 export default class Search extends Component {
 
-    state = { userName: '' };
+    state = {
+        userName: '',
+        loading: false,
+        error: ''
+    };
 
     handleChange = event => {
-        this.setState({ userName: event.target.value });
+        this.setState({ userName: event.target.value, error: '' });
     }
 
     handleSubmit = async event => {
         event.preventDefault();
 
-        api.get(`/users/${this.state.userName}`)
-            .then(res => {
-                // console.log(res);
-                this.props.history.push('/home', { 'user': res.data });
-            })
-            .catch(err => {
-                console.log(err)
-            })
+        if (this.state.userName) {
+            
+            this.setState({ loading: true })
+
+            api.get(`/users/${this.state.userName}`)
+                .then(res => {
+                    this.setState({ loading: false })
+                    this.props.history.push('/home', { 'user': res.data });
+                })
+                .catch(err => {
+                    this.setState({ loading: false });
+
+                    (err.response.status === 404) ?
+                        this.setState({ error: 'Usuário não encontrado' }) :
+                        this.setState({ error: 'Falha ao obter os dados' })
+                })
+        }
+        else {
+            this.setState({ error: 'O nickname é obrigatório' })
+        }
 
     }
 
 
 
     render() {
+        const { loading, error } = this.state
+
         return (
             <div className="login-container">
                 <form onSubmit={this.handleSubmit}>
+                    <span className="logo">Github Search</span>
                     <input type="text"
                         placeholder="Digite o usuário do github"
                         value={this.state.userName}
                         onChange={this.handleChange} />
 
-                    <button type="submit">Enviar</button>
+                    <button type="submit" disabled={loading}>
+                        {loading && <span>Buscando...</span>}
+                        {!loading && <span>Buscar</span>}
+                    </button>
+
+                    {error && <span className="login-error">{error}</span>}
                 </form>
             </div>
         );
