ab2d21039dc168737e461d85088945a815bd96fa
Tyago Nunes <tyagonunes@gmail.com>
2019-08-18 11:09:41 -0300

    back button

--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-import { FaUser, FaMapMarkerAlt, FaGithub, FaEnvelope } from 'react-icons/fa';
+import { FaUser, FaMapMarkerAlt, FaGithub, FaEnvelope, FaChevronLeft } from 'react-icons/fa';
 
 import Starreds from '../../components/Starreds/Starreds';
 import GoogleMap from '../../components/GoogleMap/GoogleMap';
@@ -14,10 +14,10 @@ export default class Main extends Component {
 	}
 
 	componentDidMount() {
-		
+
 		// Protege a rota main
 		(this.props.location.state && this.props.location.state.user) ?
-			this.setState({ user: this.props.location.state.user }):
+			this.setState({ user: this.props.location.state.user }) :
 			this.props.history.push('/');
 	}
 
@@ -25,35 +25,37 @@ export default class Main extends Component {
 		const { user } = this.state;
 
 		return (
-			<div className="main-container">
+			<div className="main">
+				<div className="main-back">
+					<Link to="/" className="back-link"> <FaChevronLeft/>Voltar</Link>
+				</div>
+				<div className="main-container">
 
-				<section className="container-profile show-slow">
+					<section className="container-profile show-slow">
 
-					<figure>
-						<img src={user.avatar_url} alt="avatar" />
-					</figure>
+						<figure>
+							<img src={user.avatar_url} alt="avatar" />
+						</figure>
 
-					<div className="profile-details">
-						<ul>
-							{user.bio && <li>{user.bio}</li>}
-							{user.login && <li><FaUser /><span className="profile-nickname">{user.login}</span></li>}
-							{user.email && <li><FaEnvelope /><span className="profile-email">{user.email}</span></li>}
-							{user.html_url && <li><FaGithub /><a className="profile-link" href={user.html_url} target="blank">{user.html_url}</a></li>}
-							{user.location && <li><FaMapMarkerAlt /><span className="profile-location">{user.location}</span></li>}
-						</ul>
-						{user.location && <GoogleMap userLocation={user.location} />}
-					</div>
-					<div className="profile-logout">
-						<Link to="/" className="btn btn-primary">
-							Pesquisar outro
-						</Link>
-					</div>
-				</section>
+						<div className="profile-details">
+							<ul>
+								{user.bio && <li>{user.bio}</li>}
+								{user.login && <li><FaUser /><span className="profile-nickname">{user.login}</span></li>}
+								{user.email && <li><FaEnvelope /><span className="profile-email">{user.email}</span></li>}
+								{user.html_url && <li><FaGithub /><a className="profile-link" href={user.html_url} target="blank">{user.html_url}</a></li>}
+								{user.location && <li><FaMapMarkerAlt /><span className="profile-location">{user.location}</span></li>}
+							</ul>
+							{user.location && <GoogleMap userLocation={user.location} />}
+						</div>
 
-				<section className="container-starreds">
-					{user.login && <Starreds userLogin={user.login} />}
-				</section>
+					</section>
+
+					<section className="container-starreds">
+						{user.login && <Starreds userLogin={user.login} />}
+					</section>
+				</div>
 			</div>
+
 		);
 	}
 }
