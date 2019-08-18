0ef2b93b254df7eb41fb6211a5cbd3fd9401380d
Tyago Nunes <tyagonunes@gmail.com>
2019-08-18 20:33:38 -0300

    refactoring Starreds

--- src/components/Starreds/Starreds.js
+++ src/components/Starreds/Starreds.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import api, {dataAuth} from '../../services/api';
+import api, { dataAuth } from '../../services/api';
 import ReactLoading from 'react-loading';
 import { FaStar } from 'react-icons/fa';
 import './Starreds.css';
@@ -13,8 +13,8 @@ export default class Starreds extends Component {
         error: '',
         userLogin: '',
         passwordLogin: '',
-        snackbaropen: false,
-        snackbarmsg: ''
+        snackbarOpen: false,
+        snackbarMsg: ''
     }
 
     componentDidMount() {
@@ -23,73 +23,70 @@ export default class Starreds extends Component {
         this.setState({ loadingStarreds: true, userLogin: userLogin })
 
         api.get(`/users/${userLogin}/starred`)
-            .then(res =>this.setState({ starreds: res.data, loadingStarreds: false }))
+            .then(res => this.setState({ starreds: res.data, loadingStarreds: false }))
             .catch(() => this.setState({ error: 'Falha em obter os dados do usuário', loadingStarreds: false }))
     }
 
 
     unstarRepo = async (owner, repo) => {
-        if(!this.state.passwordLogin) {
-            let password = await prompt('Coloque a sua senha')
-            this.setState({passwordLogin: password})
+        if (!this.state.passwordLogin) {
+            let password = await prompt('Coloque a senha desse usuário para que possamos autentica-lo')
+            this.setState({ passwordLogin: password })
         }
 
-        let auth = btoa(this.state.userLogin + ":" + this.state.passwordLogin)
+        let auth = btoa(`${this.state.userLogin}:${this.state.passwordLogin}`)
 
         api.delete(`/user/starred/${owner}/${repo}?client_id=${dataAuth.client_id}&client_secret=${dataAuth.client_secret}`, {
             headers: {
                 'authorization': `Basic ${auth}`,
             }
         })
-        .then((res) => { this.openSnackbar('Estrela removida com sucesso')})
-        .catch((err) => {
-            alert('Erro ao remover estrela do repositório. Tente novamente com a senha correta')
-            this.setState({passwordLogin: ''})
-        })
+            .then((res) => { this.openSnackbar('Estrela removida com sucesso') })
+            .catch((err) => {
+                this.openSnackbar('Erro ao remover a estrela no repositório. Tente novamente com a senha correta.')
+                this.setState({ passwordLogin: '' })
+            })
     }
 
 
     starRepo = async (owner, repo) => {
-        if(!this.state.passwordLogin) {
-            let password = await prompt('Coloque a sua senha')
-            this.setState({passwordLogin: password})
+        if (!this.state.passwordLogin) {
+            let password = await prompt('Coloque a senha desse usuário para que possamos autentica-lo')
+            this.setState({ passwordLogin: password })
         }
 
-        let auth = btoa(this.state.userLogin + ":" + this.state.passwordLogin)
+        let auth = btoa(`${this.state.userLogin}:${this.state.passwordLogin}`)
 
         api.put(`/user/starred/${owner}/${repo}?client_id=${dataAuth.client_id}&client_secret=${dataAuth.client_secret}`, {}, {
             headers: {
                 'authorization': `Basic ${auth}`,
             }
         })
-        .then((res) => { this.openSnackbar('Estrela adicionada com sucesso')})
-        .catch((err) => {
-            this.openSnackbar('Erro ao adicionar estrela no repositório. Tente novamente com a senha correta')
-            this.setState({passwordLogin: ''})
-        })
+            .then((res) => { this.openSnackbar('Estrela adicionada com sucesso') })
+            .catch((err) => {
+                this.openSnackbar('Erro ao adicionar estrela no repositório. Tente novamente com a senha correta.')
+                this.setState({ passwordLogin: '' })
+            })
     }
 
+
     openSnackbar = (message) => {
-        this.setState({snackbaropen: true, snackbarmsg: message})
+        this.setState({ snackbarOpen: true, snackbarMsg: message })
 
         setTimeout(() => {
-            this.setState({snackbaropen: false})
+            this.setState({ snackbarOpen: false })
         }, 3000)
-    } 
+    }
+
 
     render() {
         const { loadingStarreds, starreds, error } = this.state;
 
         return (
             <div className="starreds-container show-slow">
-                <Snackbar 
-                    anchorOrigin={{vertical:'bottom', horizontal:'center'}}
-                    open = {this.state.snackbaropen}
-                    autoHideDuration = {3000}
-                    message = {<span id="message-id">{this.state.snackbarmsg}</span>}
-                />
 
                 <h2>Repositórios salvos como estrela</h2>
+
                 {loadingStarreds ? (
                     <div className="starreds-loading">
                         <ReactLoading type={'bubbles'} color={'#666666'} height={100} width={100} />
@@ -104,13 +101,13 @@ export default class Starreds extends Component {
                                             <div><small>{star.description}</small></div>
 
                                             <footer>
-                                                <button className="btn" 
+                                                <button className="btn"
                                                     onClick={() => this.starRepo(star.owner.login, star.name)}>
-                                                    <FaStar/> star
+                                                    <FaStar /> star
                                                 </button>
-                                                <button className="btn" 
+                                                <button className="btn"
                                                     onClick={() => this.unstarRepo(star.owner.login, star.name)}>
-                                                    <FaStar/> unstar
+                                                    <FaStar /> unstar
                                                 </button>
                                             </footer>
                                         </li>
@@ -120,9 +117,16 @@ export default class Starreds extends Component {
                                     <div className="starreds-message">Não há repositórios salvos como estrela para esse usuário.</div>
                                 )}
 
-                            { error && <div className="starreds-message">{error}</div>}
+                            {error && <div className="starreds-message">{error}</div>}
                         </div>
                     )}
+
+                <Snackbar
+                    anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
+                    open={this.state.snackbarOpen}
+                    autoHideDuration={3000}
+                    message={<span id="message-id">{this.state.snackbarMsg}</span>}
+                />
             </div>
 
         );
